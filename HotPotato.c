002d44c662fb2f305ca2194a44241f424f1d6ee3
justinh <justin-homme@bethel.edu>
2022-04-11 00:06:32 -0500

    a few errors yet to fix

--- HotPotato.c
+++ HotPotato.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 struct Potato
 {
     short potato_count, gold_count;
     int numID;
 }; // HotPotato
+
+void passPotato();
+
+void switchGold();
 
 struct Potato *createPotato(int newNumID)
 {
@@ -25,36 +30,41 @@ void main()
     /*
     * variables
     */
-    char *usrin;
+    char usrin;
     pthread_t child_thread;
     int curIndex = 0;
     int passIndex = 0;
-    struct Potato thread_array[8];
+    int goldIndex;
+    struct Potato *thread_array[8];
     int finished = 0;                // is the program finished riunning?
 
     printf("T - thread; H - Hot Potato; G - Gold; x - exit\n");
 
     while(finished == 0) {
         scanf(" %c", &usrin);
-        switch(usrin) {
+        switch (usrin) {
             case 'T':
-                struct Potato newthread = createPotato(curIndex);
+                struct Potato *newthread = createPotato(curIndex);
                 pthread_create(&child_thread, NULL, newthread, NULL);
-                thread_array[curIndex] = newThread;
+                thread_array[curIndex] = newthread;
                 curIndex++;
+                break;
             case 'H':
                 passPotato();
+                break;
             case 'G':
                 goldIndex = rand() % curIndex + 0;
                 struct Potato *curThread = thread_array[goldIndex];
                 curThread->gold_count = curThread->gold_count + 1;
+                break;
             case 'x':
                 finished = 1;
+                break;
             default:
                 printf("You must enter one of the following characters: \n");
                 printf("T - thread; H - Hot Potato; G - Gold; x - exit\n");
-                scanf(" %c", %usrin);
-                checkInput(usrin);
+                scanf(" %c", &usrin);
+                break;
         }
 
         sleep(1);
@@ -62,7 +72,7 @@ void main()
 //      switchGold();
     }
 
-    passPotato()
+    void passPotato()
     {
         passIndex = rand() % curIndex + 0;
         struct Potato *curThread = thread_array[passIndex];
@@ -70,15 +80,9 @@ void main()
         printf("%d: Ouch! I have the hot potato!\n", passIndex);
     } // passPotato
 
-    switchGold()
+    void switchGold()
     {
         // TODO: implement the function
 
     } // switchGold
 } // main
-
-void exit()
-{
-    //TODO: implement function
-
-} // exit
